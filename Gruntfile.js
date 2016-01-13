7fa790d8c1327fdd4f8de97b6960871fc75cf424
AdrianCozma <adrian.cozma.luxoft@gmail.com>
2016-01-13 15:36:41 +0200

    work in progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function (grunt) {
                 },
                 src: ['test/testFrameworks/mocha-k3po*.js']
             },
-            testMochaK3poBrowserSupport: {
+            testMochaK3poFirefoxBrowserSupport: {
                 options: {
                     reporter: 'spec',
                     ui: 'mocha-k3po',
@@ -77,8 +77,51 @@ module.exports = function (grunt) {
                     captureFile: "build/testMochaK3po.txt",
                     browser: {
                         desiredCapabilities: {
-                            browserName: 'firefox'
-                        }
+                            browserName: 'firefox',
+                            platform: 'linux'
+                        },
+                    host: '127.0.0.1',
+                    port: '4445',
+                    user: 'kaazing-build',
+                    key: 'f350bbbb-376e-4c0e-b7b6-ae78f58f67f2'
+                    }
+                },
+                src: ['test/testFrameworks/mocha-k3po*.js']
+            },
+            testMochaK3poChromeBrowserSupport: {
+                options: {
+                    reporter: 'spec',
+                    ui: 'mocha-k3po',
+                    require: 'lib/testFrameworks/mocha-k3po.js',
+                    captureFile: "build/testMochaK3po.txt",
+                    browser: {
+                        desiredCapabilities: {
+                            browserName: 'chrome',
+                            platform: 'linux'
+                        },
+                    host: '127.0.0.1',
+                    port: '4445',
+                    user: 'kaazing-build',
+                    key: 'f350bbbb-376e-4c0e-b7b6-ae78f58f67f2'
+                    }
+                },
+                src: ['test/testFrameworks/mocha-k3po*.js']
+            },
+            testMochaK3poSafariBrowserSupport: {
+                options: {
+                    reporter: 'spec',
+                    ui: 'mocha-k3po',
+                    require: 'lib/testFrameworks/mocha-k3po.js',
+                    captureFile: "build/testMochaK3po.txt",
+                    browser: {
+                        desiredCapabilities: {
+                            browserName: 'safari',
+                            platform: 'mac'
+                        },
+                    host: '127.0.0.1',
+                    port: '4445',
+                    user: 'kaazing-build',
+                    key: 'f350bbbb-376e-4c0e-b7b6-ae78f58f67f2'
                     }
                 },
                 src: ['test/testFrameworks/mocha-k3po*.js']
@@ -90,13 +133,12 @@ module.exports = function (grunt) {
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-webdriver');
     grunt.loadNpmTasks('grunt-k3po');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.registerTask('runRobot', ['k3po:daemon']);
-    grunt.registerTask('startServer', ['connect']);
-    grunt.loadNpmTasks('grunt-webdriver');
 
     grunt.registerTask('default', ['clean', 'jshint', 'mochaTest:testBase', 'k3po:start', 'mochaTest:testMochaK3po']);
-    grunt.registerTask('firefox', ['clean', 'jshint', 'mochaTest:testBase', 'k3po:start', 'mochaTest:testMochaK3poBrowserSupport']);
+    grunt.registerTask('firefox', ['clean', 'jshint', 'mochaTest:testBase', 'k3po:start', 'mochaTest:testMochaK3poFirefoxBrowserSupport']);
+    grunt.registerTask('chrome', ['clean', 'jshint', 'mochaTest:testBase', 'k3po:start', 'mochaTest:testMochaK3poChromeBrowserSupport']);
+    grunt.registerTask('safari', ['clean', 'jshint', 'mochaTest:testBase', 'k3po:start', 'mochaTest:testMochaK3poSafariBrowserSupport']);
 };
--- test/testFrameworks/mocha-k3po_spec.js
+++ test/testFrameworks/mocha-k3po_spec.js
@@ -6,26 +6,29 @@ describe('WsClient', function () {
 
     browserConfig.origin('http://localhost:8080').addResource("http://chaijs.com/chai.js");
 
-    it('echo.from.ws.server', function (done) {
+    // it('echo.from.ws.server', function (done) {
 
-        var echoText = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345";
-        var ws = new WebSocket("ws://localhost:8080/echo");
-        ws.onopen = function () {
-            k3po.notify("CLIENT_READY_TO_READ");
-            k3po.await("SERVER_READY_TO_READ").then(function () {
-                ws.send(echoText);
-            });
-        };
+    //     var echoText = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345";
+    //     var ws = new WebSocket("ws://localhost:8080/echo");
+    //     ws.onopen = function () {
+    //         k3po.notify("CLIENT_READY_TO_READ");
+    //         k3po.await("SERVER_READY_TO_READ").then(function () {
+    //             ws.send(echoText);
+    //         });
+    //     };
 
-        ws.onmessage = function (event) {
-            chai.assert.equal(event.data, echoText);
-            ws.close();
-        };
+    //     ws.onmessage = function (event) {
+    //         chai.assert.equal(event.data, echoText);
+    //         ws.close();
+    //     };
 
-        ws.onclose = ws.onerror = function () {
-            done();
-        };
-    });
+    //     ws.onclose = ws.onerror = function () {
+    //     };
+
+    //     k3po.finish().then(function () {
+    //         done();
+    //     });
+    // });
 
     it('echo.from.ws.server', function (done) {
         var echoText = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345";
